8a3fd959ab8273f376d5c9c64591d9947a56351b
Jeff <jeff.uca@gmail.com>
2019-08-25 20:03:27 +0200

    Agregado algoritmo orden topológico

---
+++ orden_topologico.py
@@ -0,0 +1,37 @@
+def profundidad(l, i, m, v,k):
+    m[i] = True
+
+    for i in l:
+        if not m[i]:
+            profundidad(l,i,m,v,k)
+
+    v[k] = i
+    k = k -1
+
+    return m ,v, k
+
+
+def orden_topologico(lista_adyacencia, numero_vertices):
+
+    visitados =[False]*numero_vertices
+    solucion =[0] *numero_vertices
+
+    k = numero_vertices
+
+    for i in range(0, numero_vertices):
+
+        if not visitados[i]:
+            visitados,solucion,k = profundidad(lista_adyacencia, i, visitados,solucion, k)
+
+    return solucion
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # TODO implement
+  pass
